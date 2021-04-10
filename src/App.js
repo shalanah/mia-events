86ef4368b17795f7756e1d7837b9304c01fa5cef
shalanah <shalanahfaith@gmail.com>
2021-04-10 15:41:25 -0500

    Write a basic reset + create file for events

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+const CSSReset = createGlobalStyle`
+  body, html {
+    width: 100%;
+    height: 100%;
+  }
+  *, *:after, *:before {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
 function App() {
-  return <div>Hey</div>;
+  return (
+    <>
+      <CSSReset />
+      <div>Hey</div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/events.js
@@ -0,0 +1,11 @@
+// Let's start with some temp data
+// TODO: Don't forget to handle the "empty" state for each, usually there won't be an "empty" for past events but for future + current there could be
+
+export const events = [
+  {
+    title: "Temp Data 1",
+    date: "TBD on format",
+    description: "More on this",
+    img: "url here",
+  },
+];
